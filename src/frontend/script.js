a1283dcb6098c93bc4e3d1fa373898cee37fb1f2
Rishab Maheshwari <rishabmahesh@umass.edu>
2022-12-16 02:07:35 -0500

    edited geocoder

--- src/frontend/script.js
+++ src/frontend/script.js
@@ -12,20 +12,17 @@ function setupMap(center) {
   });
 
   // Add geolocate control to the map.
-  map.addControl(
-    new mapboxgl.GeolocateControl({
-      positionOptions: {
-        enableHighAccuracy: true
-      },
-      // When active the map will receive updates to the device's location as it changes.
-      trackUserLocation: true,
-      // Draw an arrow next to the location dot to indicate which direction the device is heading.
-      showUserHeading: true
-    }), "top-right"
-  );
+  const geolocate = new mapboxgl.GeolocateControl({
+    positionOptions: {
+      enableHighAccuracy: true,
+    },
+    trackUserLocation: true,
+    showUserHeading: true,
+  });
 
   const nav = new mapboxgl.NavigationControl()
   map.addControl(nav)
+  map.addControl(geolocate)
 
   var directions = new MapboxDirections({
     accessToken: mapboxgl.accessToken
